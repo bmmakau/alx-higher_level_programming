89605d9c71cb55ae5daba98fdc89306556900bf7
BM Makau <bmmakau74@gmail.com>
2023-01-05 10:52:14 -0800

    python-if_else_loops_functions task 1 correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,18 +5,18 @@ number = random.randint(-10000, 10000)
 last_string = str(number)[-1]
 if number > 0:
     if int(last_string) > 5:
-        print("Last digit of {} is {} and is greater than \
-            5".format(number, last_string,))
+        stmt = "Last digit of {} is {} and is greater than 5"
+        print(stmt.format(number, last_string,))
     elif int(last_string) == 0:
-        print("Last digit of {} is {} and is \
-            0".format(number, last_string,))
+        stmt = "Last digit of {} is {} and is 0"
+        print(stmt.format(number, last_string,))
     else:
-        print("Last digit of {} is {} and is less than \
-            6 and not 0".format(number, last_string,))
+        stmt = "Last digit of {} is {} and is less than 6 and not 0"
+        print(stmt.format(number, last_string,))
 elif number == 0:
-    print("Last digit of {} is {} and is \
-        0".format(number, last_string,))
+    stmt = "Last digit of {} is {} and is 0"
+    print(stmt.format(number, last_string,))
 else:
     last_string = int(last_string) * -1
-    print("Last digit of {} is {} and is less than \
-        6 and not 0".format(number, last_string,))
+    stmt = "Last digit of {} is {} and is less than 6 and not 0"
+    print(stmt.format(number, last_string,))
